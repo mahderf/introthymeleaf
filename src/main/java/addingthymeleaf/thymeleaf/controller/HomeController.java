22a71ed26dc3855b96b756c635341026e2b70f01
student <mymahder@gmail.com>
2017-08-01 18:15:59 -0400

    Initial commit

---
+++ src/main/java/addingthymeleaf/thymeleaf/controller/HomeController.java
@@ -0,0 +1,14 @@
+package addingthymeleaf.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("message","Hello");
+        return "hello";
+    }
+}
